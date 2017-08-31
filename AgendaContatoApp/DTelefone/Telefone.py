839d41c6f30fd22bccb169d8b559c141df9c330a
maria clara ferreira rodrigues <mariaclarafr01@gmail.com>
2017-08-30 21:37:59 -0300

    Update Telefone.py

--- AgendaContatoApp/DTelefone/Telefone.py
+++ AgendaContatoApp/DTelefone/Telefone.py
@@ -1,7 +1,14 @@
-import json
 class Telefone():
-    def __init__(self, numero, DDD, codPais):
-        self.numero = numero
-        self.DDD = DDD
-        self.codPais = codPais
+    try:
 
+        def __init__(self, numero, DDD, codPais):
+            self.numero = numero
+            self.DDD = DDD
+            self.codPais = codPais
+
+        def __str__(self):
+            return ("+ %i ( %i )%s" % self.codPais, self.DDD, self.numero)
+
+
+    except:
+        print("erro na classe Telefone")
