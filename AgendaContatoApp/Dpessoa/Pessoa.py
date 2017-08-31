3358f5f7ef725a8e6bd7a88392c1c4fdf4d49f64
maria clara ferreira rodrigues <mariaclarafr01@gmail.com>
2017-08-30 21:39:15 -0300

    Update Pessoa.py

--- AgendaContatoApp/Dpessoa/Pessoa.py
+++ AgendaContatoApp/Dpessoa/Pessoa.py
@@ -1,7 +1,11 @@
-import json
-class pessoa():
-    def __init__(self, nome, nascimento, email):
-        self.nome = nome
-        self.nascimento = nascimento
-        self.email = email
-        
+class Pessoa():
+    try:
+        def __init__(self, nome, nascimento, email):
+            self.nome = nome
+            self.nascimento = nascimento
+            self.email = email
+
+        def __str__(self):
+            return "%s , %s , %s" % (self.nome + self.nascimento + self.email)
+    except:
+        print("erro na classe Pessoa.")
